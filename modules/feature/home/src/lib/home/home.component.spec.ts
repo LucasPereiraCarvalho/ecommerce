e2571b1f5e6c0cf55629e25bc6c209f7799c1ac9
Lucas Pereira <lucas.carvalho@copel.com>
2024-02-28 23:16:56 -0300

    feat(home): add recommended products

--- modules/feature/home/src/lib/home/home.component.spec.ts
+++ modules/feature/home/src/lib/home/home.component.spec.ts
@@ -1,12 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
-
+import { MatCardModule } from '@angular/material/card';
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [MatCardModule],
       declarations: [HomeComponent],
     }).compileComponents();
 
